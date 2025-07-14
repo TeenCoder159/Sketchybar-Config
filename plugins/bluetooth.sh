c192800f73fa32db8b15e6d53ef505f35a60aaf4
TeenCoder159 <no1umang@gmail.com>
2025-07-14 15:12:14 +0800

    Update colours to use kanagawa theme

--- plugins/bluetooth.sh
+++ plugins/bluetooth.sh
@@ -1,29 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-# nothingness: 2C:BE:EB:0B:1E:0F
+DEVICE="2C:BE:EB:0B:1E:0F"
+CONNECTED_ICON="󰂯"   # Nerd Font Bluetooth icon connected
+DISCONNECTED_ICON="󰂲" # Nerd Font Bluetooth icon disconnected
+HIGHLIGHT_COLOR="0xffD27E99"  # Kanagawa SakuraPink
+DEFAULT_COLOR="0xff7E9CD8"    # Kanagawa WaveBlue1
 
 update() {
-  if [ "$(blueutil --is-connected 2C:BE:EB:0B:1E:0F)" = "1" ]; then
-    sketchybar --set "$NAME" icon=C icon.color=0xFFFCB103
+  if [ "$(blueutil --is-connected "$DEVICE")" = "1" ]; then
+    sketchybar --set "$NAME" icon="$CONNECTED_ICON" icon.color="$HIGHLIGHT_COLOR"
   else
-    sketchybar --set "$NAME" icon=D icon.color=0xff9b9b9b
+    sketchybar --set "$NAME" icon="$DISCONNECTED_ICON" icon.color="$DEFAULT_COLOR"
   fi
 }
 
 mouse_clicked() {
-  if [ "$(blueutil --is-connected 2C:BE:EB:0B:1E:0F)" = "1" ]; then
+  if [ "$(blueutil --is-connected "$DEVICE")" = "1" ]; then
     osascript -e 'tell application "Spotify" to playpause'
-    blueutil --disconnect 2C:BE:EB:0B:1E:0F 
+    blueutil --disconnect "$DEVICE"
   else
-    blueutil --connect 2C:BE:EB:0B:1E:0F 
+    blueutil --connect "$DEVICE"
   fi
-
 }
 
-
 case "$SENDER" in
-  "mouse.clicked") mouse_clicked
-  ;;
-  *) update
-  ;;
+  "mouse.clicked") mouse_clicked ;;
+  *) update ;;
 esac
